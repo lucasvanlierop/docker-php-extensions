4bbcb0855afd947f2d45086126ee8c2f99ad31ca
Roland Franssen <franssen.roland@gmail.com>
2022-07-14 11:19:31 +0200

    fix

--- 7.4/bullseye/gd/install_requirements.sh
+++ 7.4/bullseye/gd/install_requirements.sh
@@ -2,9 +2,8 @@
 
 apt-get update && \
 apt-get install -y --no-install-recommends \
-libfreetype6=2.10.4+dfsg-1 \
+libfreetype6 \
 libpng16-16=1.6.37-3 \
 libjpeg62-turbo=1:2.0.6-4 && \
 rm -rf /var/lib/apt/lists/*
-
 
